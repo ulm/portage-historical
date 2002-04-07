8c52d88a2406607e1d4890579aa012e58668851b
Daniel Robbins <drobbins@gentoo.org>
2002-04-07 05:27:22 +0000

    1.8.17

--- src/sandbox/sandbox.c
+++ src/sandbox/sandbox.c
@@ -11,7 +11,7 @@
 **	Copyright (C) 2001 Geert Bevin, Uwyn, http://www.uwyn.com
 **	Distributed under the terms of the GNU General Public License, v2 or later 
 **	Author : Geert Bevin <gbevin@uwyn.com>
-**  $Header: /local/data/ulm/cvs/history/var/cvsroot/gentoo-src/portage/src/sandbox/Attic/sandbox.c,v 1.6 2002/03/06 09:51:02 gbevin Exp $
+**  $Header: /local/data/ulm/cvs/history/var/cvsroot/gentoo-src/portage/src/sandbox/Attic/sandbox.c,v 1.7 2002/04/07 05:27:22 drobbins Exp $
 */
 
 #define _GNU_SOURCE
@@ -630,7 +630,7 @@ int main(int argc, char** argv)
 			{
 				/* these should go into make.globals later on */
 				strcpy(sandbox_write_var, "");
-				strcat(sandbox_write_var, "/dev/null:/dev/pts/:/dev/tty:/tmp/");
+				strcat(sandbox_write_var, "/dev/null:/dev/pts/:/dev/vc/:/dev/tty:/tmp/");
 				strcat(sandbox_write_var, ":");
 				strcat(sandbox_write_var, "/var/log/scrollkeeper.log");
 				strcat(sandbox_write_var, ":");
