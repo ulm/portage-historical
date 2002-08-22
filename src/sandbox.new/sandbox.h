6af72175054e032b9cdcd45f2689a0038d62a094
Martin Schlemmer <azarah@gentoo.org>
2002-08-22 22:13:04 +0000

    form fixes

--- src/sandbox.new/sandbox.h
+++ src/sandbox.new/sandbox.h
@@ -1,5 +1,10 @@
-/*
- * $Header: /local/data/ulm/cvs/history/var/cvsroot/gentoo-src/portage/src/sandbox.new/Attic/sandbox.h,v 1.3 2002/08/20 08:03:55 azarah Exp $
+/* 
+ * Copyright (C) 2002 Brad House <brad@mainstreetsoftworks.com>,
+ * Possibly based on code from Geert Bevin, Uwyn, http://www.uwyn.com
+ * Distributed under the terms of the GNU General Public License, v2 or later 
+ * Author: Brad House <brad@mainstreetsoftworks.com>
+ *    
+ * $Header: /local/data/ulm/cvs/history/var/cvsroot/gentoo-src/portage/src/sandbox.new/Attic/sandbox.h,v 1.4 2002/08/22 22:13:04 azarah Exp $
  */
 
 #ifndef __SANDBOX_H__
--- src/sandbox.new/sandbox_futils.c
+++ src/sandbox.new/sandbox_futils.c
@@ -1,5 +1,10 @@
 /*
- * $Header: /local/data/ulm/cvs/history/var/cvsroot/gentoo-src/portage/src/sandbox.new/Attic/sandbox_futils.c,v 1.3 2002/08/20 08:03:55 azarah Exp $
+ * Copyright (C) 2002 Brad House <brad@mainstreetsoftworks.com>
+ * Distributed under the terms of the GNU General Public License, v2 or later 
+ * Author: Brad House <brad@mainstreetsoftworks.com>
+ *
+ * $Header: /local/data/ulm/cvs/history/var/cvsroot/gentoo-src/portage/src/sandbox.new/Attic/sandbox_futils.c,v 1.4 2002/08/22 22:13:04 azarah Exp $
+ * 
  */
 
 #include <errno.h>
