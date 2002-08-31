ef6ae172b5222a77e55467ab5eda228f439d9ac1
Martin Schlemmer <azarah@gentoo.org>
2002-08-31 19:32:19 +0000

    remove pidsfile if last sandbox

--- src/sandbox-1.1/sandbox.c
+++ src/sandbox-1.1/sandbox.c
@@ -11,7 +11,7 @@
 **	Copyright (C) 2001 Geert Bevin, Uwyn, http://www.uwyn.com
 **	Distributed under the terms of the GNU General Public License, v2 or later 
 **	Author : Geert Bevin <gbevin@uwyn.com>
-**  $Header: /local/data/ulm/cvs/history/var/cvsroot/gentoo-src/portage/src/sandbox-1.1/Attic/sandbox.c,v 1.2 2002/08/26 19:40:31 azarah Exp $
+**  $Header: /local/data/ulm/cvs/history/var/cvsroot/gentoo-src/portage/src/sandbox-1.1/Attic/sandbox.c,v 1.3 2002/08/31 19:32:19 azarah Exp $
 */
 
 #define _GNU_SOURCE
@@ -238,15 +238,22 @@ void cleanup()
           break;
         }
       }
+
+      file_close(pids_file);
+      pids_file = -1;
+    } else {
+            
+      file_close(pids_file);
+      pids_file = -1;
+
+      /* remove the pidsfile, as this was the last sandbox */
+      unlink(PIDS_FILE);
     }
 
     if (pids_array != NULL) {
       free(pids_array);
       pids_array = NULL;
     }
-
-    file_close(pids_file);
-    pids_file = -1;
   }
 
   if (0 == success) {
