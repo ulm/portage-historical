9e8fe22c1209ea1338303b49d252b94e4441d289
Nicholas Jones <carpaski@gentoo.org>
2004-02-06 21:12:34 +0000

    this is portage-2.0.50

--- bin/regenworld.sh
+++
@@ -1,8 +0,0 @@
-# $Header: /local/data/ulm/cvs/history/var/cvsroot/gentoo-src/portage/bin/Attic/regenworld.sh,v 1.2 2003/02/16 03:03:41 carpaski Exp $
-
-egrep '^[0-9]+:  \*\*\* emerge' /var/log/emerge.log |
-egrep -v 'oneshot|nodeps|emerge .* search ' |
-sed 's:^.*\* emerge ::;s:--[^ ]\+ ::;s: :\n:g' |
-egrep '^[a-zA-Z=><]' |
-egrep -v '^[0-9]|\.ebuild$|\.tbz2$' |
-sort -u
