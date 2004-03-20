8171f6fff7c131d1ddc42088ae8378a7e6092702
Nicholas Jones <carpaski@gentoo.org>
2004-03-20 23:48:43 +0000

    This would make the environment huge... so we're going to forego having them ever in the environment.

--- bin/functions/dodir.sh
+++
@@ -1,9 +0,0 @@
-# Copyright 1999-2003 Gentoo Technologies, Inc.
-# Distributed under the terms of the GNU General Public License v2
-# $Header: /local/data/ulm/cvs/history/var/cvsroot/gentoo-src/portage/bin/functions/Attic/dodir.sh,v 1.1 2003/12/10 06:00:56 carpaski Exp $
-
-dodir() {
-	for x in "$@" ; do
-		install -d ${DIROPTIONS} "${D}${x}" || die
-	done
-}
