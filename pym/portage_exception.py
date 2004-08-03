a12438d9fd851d00bfd0cf3486d07f174acbd314
Nicholas Jones <carpaski@gentoo.org>
2004-08-03 21:34:00 +0000

    Notification on invalid depstrings for ||.

--- pym/portage_exception.py
+++ pym/portage_exception.py
@@ -1,7 +1,14 @@
-# $Header: /local/data/ulm/cvs/history/var/cvsroot/gentoo-src/portage/pym/portage_exception.py,v 1.1 2004/05/17 04:21:21 carpaski Exp $
+# $Header: /local/data/ulm/cvs/history/var/cvsroot/gentoo-src/portage/pym/portage_exception.py,v 1.2 2004/08/03 21:34:00 carpaski Exp $
 
 class CorruptionError(Exception):
 	"""Corruption indication"""
+	def __init__(self,value):
+		self.value = value[:]
+	def __str__(self):
+		return repr(self.value)
+
+class InvalidDependString(Exception):
+	"""An invalid depend string has been encountered"""
 	def __init__(self,value):
 		self.value = value[:]
 	def __str__(self):
