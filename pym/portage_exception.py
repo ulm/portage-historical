079a41f5fa05a3d41300c62f235c3a27f68a3f2b
Nicholas Jones <carpaski@gentoo.org>
2004-08-15 05:33:57 +0000

    GPG verification and some migration of code to smaller modules.

--- pym/portage_exception.py
+++ pym/portage_exception.py
@@ -1,4 +1,4 @@
-# $Header: /local/data/ulm/cvs/history/var/cvsroot/gentoo-src/portage/pym/portage_exception.py,v 1.2 2004/08/03 21:34:00 carpaski Exp $
+# $Header: /local/data/ulm/cvs/history/var/cvsroot/gentoo-src/portage/pym/portage_exception.py,v 1.3 2004/08/15 05:33:57 carpaski Exp $
 
 class CorruptionError(Exception):
 	"""Corruption indication"""
@@ -9,6 +9,76 @@ class CorruptionError(Exception):
 
 class InvalidDependString(Exception):
 	"""An invalid depend string has been encountered"""
+	def __init__(self,value):
+		self.value = value[:]
+	def __str__(self):
+		return repr(self.value)
+
+class SecurityViolation(Exception):
+	"""An incorrect formatting was passed instead of the expected one"""
+	def __init__(self,value):
+		self.value = value[:]
+	def __str__(self):
+		return repr(self.value)
+
+class MissingParameter(Exception):
+	"""An parameter is required for the action requested but was not passed"""
+	def __init__(self,value):
+		self.value = value[:]
+	def __str__(self):
+		return repr(self.value)
+
+class InvalidData(Exception):
+	"""An incorrect formatting was passed instead of the expected one"""
+	def __init__(self,value):
+		self.value = value[:]
+	def __str__(self):
+		return repr(self.value)
+
+class InvalidDataType(Exception):
+	"""An incorrect type was passed instead of the expected one"""
+	def __init__(self,value):
+		self.value = value[:]
+	def __str__(self):
+		return repr(self.value)
+
+class FileNotFound(Exception):
+	"""A file was not found when it was expected to exist"""
+	def __init__(self,value):
+		self.value = value[:]
+	def __str__(self):
+		return repr(self.value)
+
+class DirectoryNotFound(Exception):
+	"""A directory was not found when it was expected to exist"""
+	def __init__(self,value):
+		self.value = value[:]
+	def __str__(self):
+		return repr(self.value)
+
+class CommandNotFound(Exception):
+	"""A required binary was not available or executable"""
+	def __init__(self,value):
+		self.value = value[:]
+	def __str__(self):
+		return repr(self.value)
+
+class MissingSignature(Exception):
+	"""Signature was not present in the checked file"""
+	def __init__(self,value):
+		self.value = value[:]
+	def __str__(self):
+		return repr(self.value)
+
+class InvalidSignature(Exception):
+	"""Signature was checked and was not a valid, current, nor trusted signature"""
+	def __init__(self,value):
+		self.value = value[:]
+	def __str__(self):
+		return repr(self.value)
+
+class UntrustedSignature(Exception):
+	"""Signature was not certified to the desired security level"""
 	def __init__(self,value):
 		self.value = value[:]
 	def __str__(self):
