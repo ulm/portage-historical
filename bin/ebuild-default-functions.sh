78b520296698151ed6b01d4012ac3492bb5250f9
Marius Mauch <genone@gentoo.org>
2004-12-15 11:50:21 +0000

    portage GID warning disabled

--- bin/ebuild-default-functions.sh
+++ bin/ebuild-default-functions.sh
@@ -2,7 +2,7 @@
 # ebuild-default-functions.sh; default functions for ebuild env that aren't saved- specific to the portage instance.
 # Copyright 2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-$Header: /local/data/ulm/cvs/history/var/cvsroot/gentoo-src/portage/bin/ebuild-default-functions.sh,v 1.15 2004/12/09 05:46:55 genone Exp $
+$Header: /local/data/ulm/cvs/history/var/cvsroot/gentoo-src/portage/bin/ebuild-default-functions.sh,v 1.16 2004/12/15 11:50:21 genone Exp $
 
 has_version() {
 	# if there is a predefined portageq call, use it.
@@ -455,21 +455,16 @@ dyn_install() {
 		[ -h "$file" ] || chmod "$s" "$file"
 	done
 
-	if [ "$USERLAND" == "BSD" ]; then
-		find "${D}/" -group portage -print | while read file; do
-			ewarn "file $file was installed with group portage!"
-			s=$(stat_perms "$file")
+	find "${D}/" -group portage -print | while read file; do
+		# Too annoying
+		#ewarn "file $file was installed with group portage!"
+		s=$(stat_perms "$file")
+		if [ "$USERLAND" == "BSD" ]; then
 			chgrp wheel "$file"
-			[ -h "$file" ] || chmod "$s" "$file"
-		done
-	else
-		find "${D}/" -group portage -print | while read file; do
-			ewarn "file $file was installed with group portage!"
-			s=$(stat_perms "$file")
+		else
 			chgrp root "$file"
-			[ -h "$file" ] || chmod "$s" "$file"
-		done
-	fi
+		[ -h "$file" ] || chmod "$s" "$file"
+	done
 
 	echo ">>> Completed installing into ${D}"
 	echo
