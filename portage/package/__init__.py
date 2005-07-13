0c700c9c9e3016440ce3506eba369f3fb2567704
Brian Harring <ferringb@gentoo.org>
2005-07-13 05:55:39 +0000

    bleh, this has been bugging me for weeks... removed the from cpv import CPV as cpv to just import CPV

--- portage/package/__init__.py
+++ portage/package/__init__.py
@@ -1,7 +1,8 @@
 # Copyright: 2005 Gentoo Foundation
 # Author(s): Brian Harring (ferringb@gentoo.org)
 # License: GPL2
-# $Header: /local/data/ulm/cvs/history/var/cvsroot/gentoo-src/portage/portage/package/__init__.py,v 1.2 2005/07/13 05:51:35 ferringb Exp $
+# $Header: /local/data/ulm/cvs/history/var/cvsroot/gentoo-src/portage/portage/package/__init__.py,v 1.3 2005/07/13 05:55:39 ferringb Exp $
 
 import metadata
-from cpv import CPV as cpv
+import cpv
+import atom
